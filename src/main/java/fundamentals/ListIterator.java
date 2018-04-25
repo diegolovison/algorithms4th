1e650ff6f80b36bc6424437781690cb8aa07fc46
diegolovison <diegolovison@gmail.com>
2018-04-24 21:10:12 -0300

    List Iterator

---
+++ src/main/java/fundamentals/ListIterator.java
@@ -0,0 +1,29 @@
+package fundamentals;
+
+import java.util.Iterator;
+
+public class ListIterator<Item> implements Iterator<Item> {
+
+    private Node<Item> current;
+
+    public ListIterator(Node item) {
+        this.current = item;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return current != null;
+    }
+
+    @Override
+    public Item next() {
+        Item item = current.item;
+        current = current.next;
+        return item;
+    }
+
+    static class Node<Item> {
+        Item item;
+        Node next;
+    }
+}
